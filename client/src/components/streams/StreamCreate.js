6ff508ceb47f54642839b1e60d0c2e5d088c6818
anjana6 <anjanashakthi95@gmail.com>
2019-11-10 05:31:58 +0530

    create reducers

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field,reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import {createStream} from '../../actions';
 
 class StreamCreate extends React.Component{
     renderError({error,touched}){
@@ -24,8 +26,9 @@ class StreamCreate extends React.Component{
             );
     }
 
-    onSubmit(formValues){
-        console.log(formValues);
+    onSubmit = (formValues) =>{
+        // console.log(formValues);
+        this.props.createStream(formValues);
     }
     
     render(){
@@ -37,7 +40,7 @@ class StreamCreate extends React.Component{
             <button className="ui button primary">Submit</button>
         </form>
     );
-    }
+    } 
 }
 
 const validate = formValues => {
@@ -53,7 +56,9 @@ const validate = formValues => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null,{createStream})(formWrapped);
